825aeb0b6beb8f7bcbcd31c37d3e8de0b1b84468
Justin Kirk <thejuki@hotmail.com>
2015-10-20 14:36:13 -0500

    Code Blocks Initial

---
+++ Networking Project/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << endl << " CSC 311 Networking Project Group 7" << endl;
+    cout << endl << " If you are reading this then you having a running project!" << endl;
+
+    string s;
+     cout << endl << endl << endl<< " Press Enter to exit";
+    getline(cin, s);
+
+    return 0;
+}
